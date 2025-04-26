ae7a642043afa0bd2fefcac6e45176e7cc4264fc
arq0n <71539626+Arq0n@users.noreply.github.com>
2025-04-26 01:26:42 -0700

    Revert "Revert "implement login page""
    This reverts commit 63bca5b435a503116fd1a5f51864ecdc85decac0.

--- server/app.js
+++ server/app.js
@@ -1,24 +1,35 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { schoolRouter } from "./routes/schools.js";
+import loginRoute from './routes/login.js';
 
 dotenv.config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
-  }));
-app.use(express.json()); // Good to include for parsing JSON requests
+}));
+app.use(express.json());
 
 app.use("/school", schoolRouter);
+app.use('/auth/login', loginRoute);
 
 app.get('/', (req, res) => {
-res.send('API is running');
+    res.send('API is running');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/public/login.html'));
 });
 
 app.listen(3001, () => {
     console.log('server listening on 3001');
-})
+});
