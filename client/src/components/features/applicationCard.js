2c68da71844e843a6da65d633981f567db0b3604
Lindsay Lim <limlindsayj@gmail.com>
2025-04-26 15:39:06 -0700

    styling

--- client/src/components/features/applicationCard.js
+++ client/src/components/features/applicationCard.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardHeader, CardBody, Flex, Heading, Tag, Text, HStack, Box } from '@chakra-ui/react';
 import { LucideThumbsUp, MessageCircle } from 'lucide-react'; // Import icons
 
 function ApplicationCard({ application }) {
+    const [admitColor, setAdmitColor] = useState('');
+    const [admitBorder, setAdmitBorder] = useState('');
+
+    useEffect(() => {
+        const fetchColorCondition = () => {  
+            if (application.admit_status === "Accepted") {
+                setAdmitColor('var(--green-100, #C6F6D5)');
+                setAdmitBorder('var(--green-800, #22543D)')
+            } else if (application.admit_status === "Waitlisted") {
+                setAdmitColor('var(--orange-100, #FEEBCB)');
+                setAdmitBorder('var(--orange-800, #7B341E)')
+            } else {
+                setAdmitColor('var(--red-100, #FED7D7)');
+                setAdmitBorder('var(--red-800, #822727)');
+            }
+        }
+    
+        fetchColorCondition();
+      }, []);
   return (
     <Card
       boxShadow="md"
@@ -22,9 +41,6 @@ function ApplicationCard({ application }) {
       </CardHeader>
       <CardBody width="100%" alignItems="center">
         <Flex width="100%" justifyContent="center" gap={4} alignItems="center">
-          <Text>
-            <strong>GPA:</strong> {application.student.gpa}
-          </Text>
           <Tag
             variant="solid"
             rounded="full"
@@ -36,9 +52,29 @@ function ApplicationCard({ application }) {
             gap="8px"
             flexShrink="0"
             borderRadius="6px"
-            backgroundColor="var(--red-100, #FED7D7)"
-            color="var(--red-800, #822727)"
-            font-size="14px"
+            backgroundColor="var(--purple-100, #E9D8FD)"
+            color="var(--purple-800, #44337A)"
+            fontSize="14px"
+            fontStyle="normal"
+            fontWeight="700"
+            lineHeight="normal"
+          >
+            <strong>GPA:</strong> {application.student.gpa}
+          </Tag>
+          <Tag
+            variant="solid"
+            rounded="full"
+            py={1}
+            display="inline-flex"
+            height="24px"
+            px="8px"
+            alignItems="center"
+            gap="8px"
+            flexShrink="0"
+            borderRadius="6px"
+            backgroundColor={admitColor}
+            color={admitBorder}
+            fontSize="14px"
             fontStyle="normal"
             fontWeight="700"
             lineHeight="normal"
@@ -59,7 +95,7 @@ function ApplicationCard({ application }) {
             borderRadius="6px"
             backgroundColor="var(--blue-100, #BEE3F8)"
             color="var(--blue-800, #2A4365)"
-            font-size="14px"
+            fontSize="14px"
             fontStyle="normal"
             fontWeight="700"
             lineHeight="normal"
