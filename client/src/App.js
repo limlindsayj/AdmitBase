465258c47add0c84cb66558decccaa1e35a6e1b9
Lindsay Lim <limlindsayj@gmail.com>
2025-04-25 21:27:07 -0700

    cors connection client+server

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,19 @@ function App() {
   
   //test call
   const apiCall = () => {
-    axios.get('http://localhost:8080').then(() => {
-      console.log('woo');
+    axios.get('http://localhost:8080').then((data) => {
+      console.log(data);
     })
   }
   return (
     <div className="App">
       <header className="App-header">
-        
       </header>
+      <main>
+        <button onClick={apiCall}>
+          Test
+        </button>
+      </main>
     </div>
   );
 }
