50cc947a14f7166509f402a5a64acd0ef391414f
Lindsay Lim <limlindsayj@gmail.com>
2025-04-25 21:11:43 -0700

    client + server connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  
+  //test call
+  const apiCall = () => {
+    axios.get('http://localhost:8080').then(() => {
+      console.log('woo');
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
