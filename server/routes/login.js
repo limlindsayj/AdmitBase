e37a42de7c403fd2a964f2fb7cdba0f2be024566
arq0n <71539626+Arq0n@users.noreply.github.com>
2025-04-26 01:23:42 -0700

    implement login page

---
+++ server/routes/login.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { db } from "../db/db-supabase.js";
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+  console.log('Request body:', req.body);
+
+  try {
+    const { data: user, error } = await db
+      .from('student')
+      .select('*')
+      .eq('email', email)
+      .single();
+
+    if (error) {
+      console.error('Database error:', error);
+      return res.status(500).json({ message: 'Database error', error });
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (password !== user.password) {
+      return res.status(400).json({ message: 'Incorrect password' });
+    }
+
+    res.json({
+      message: 'Login successful!',
+      user: {
+        id: user.id,
+        email: user.email
+      }
+    });
+
+  } catch (err) {
+    console.error('Server error:', err);
+    res.status(500).json({ message: 'Server error', error: err.message });
+  }
+});
+
+export default router;
